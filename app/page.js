b9d731d1b8e9ffb2ac4c67302cd12a5f903c86b3
tydevilish <meawpain12@gmail.com>
2025-07-21 21:14:43 +0700

    fix

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
-import Cards from "./components/Card";
+import Card from "./components/Card";
 import Carousel from "./components/Carousel";
+
 
 export default function Home() {
   return (
      <>
-      <Carousel />
-      <Cards />
+  <Card />
+  <Carousel />
     </>
   );
 }
